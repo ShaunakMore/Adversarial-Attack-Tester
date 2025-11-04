4eb2cb9e65211fbb564f597a4b0b739e8e00ebec
ShaunakMore <shaunaksmore999@gmail.com>
2025-11-04 22:10:57 +0530

    Added FGSM targeted

--- tester.py
+++ tester.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch.nn as nn
 import torch
 
 def fgsm_attack(model,image,label,epsilon=0.03, device = None):
@@ -28,3 +29,21 @@ def fgsm_attack(model,image,label,epsilon=0.03, device = None):
   perturbed_image = torch.clamp(perturbed_image,0,1)
   
   return perturbed_image.detach()
+
+def fgsm_targeted(model, image, target_class, epsilon=0.03):
+ 
+    image.requires_grad = True
+    
+    output = model(image)
+    
+    loss_fn = nn.CrossEntropyLoss()
+    
+    loss = loss_fn(output, target_class)
+    
+    model.zero_grad()
+    loss.backward()
+    
+    perturbation = -epsilon * image.grad.sign()
+    
+    perturbed = torch.clamp(image + perturbation, 0, 1)
+    return perturbed.detach()
